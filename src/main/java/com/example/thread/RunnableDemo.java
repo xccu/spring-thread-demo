5039e8794fa7297a9735f3264a2fe19095d6b326
xccu <264672790@qq.com>
2021-11-14 14:29:13 +0800

    upload

---
+++ src/main/java/com/example/thread/RunnableDemo.java
@@ -0,0 +1,45 @@
+package com.example.thread;
+
+class RunnableDemo implements Runnable {
+    private Thread t;
+    private String threadName;
+
+    RunnableDemo(String name) {
+        threadName = name;
+        System.out.println("Creating " +  threadName );
+    }
+
+    public void run() {
+        System.out.println("Running " +  threadName );
+        try {
+            for(int i = 4; i > 0; i--) {
+                System.out.println("Thread: " + threadName + ", " + i);
+                // 让线程睡眠一会
+                Thread.sleep(50);
+            }
+        }catch (InterruptedException e) {
+            System.out.println("Thread " +  threadName + " interrupted.");
+        }
+        System.out.println("Thread " +  threadName + " exiting.");
+    }
+
+    //必须通过Thread.start方法启动线程
+    public void start () {
+        System.out.println("Starting " +  threadName );
+        if (t == null) {
+            t = new Thread (this, threadName);
+            t.start ();
+        }
+    }
+}
+
+class TestRunnableDemo {
+
+    public static void main(String args[]) {
+        RunnableDemo R1 = new RunnableDemo( "Thread-1");
+        R1.start();
+
+        RunnableDemo R2 = new RunnableDemo( "Thread-2");
+        R2.start();
+    }
+}
